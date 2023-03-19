30e9a0d29fb8ad5777cd4b31bd79d3113d123f12
Mati <matimotof@gmail.com>
2023-03-19 11:24:22 +0000

    Added address_table and foreign key to borrower_table

--- borrower_loan.sql
+++ borrower_loan.sql
@@ -1,12 +1,23 @@
 use library;
 
+CREATE TABLE address
+(address_id int not null auto_increment primary key,
+house_number int,
+building varchar(50),
+street varchar(100),
+city varchar(50),
+postcode varchar(10)
+);
+
 CREATE TABLE borrower
 
 (borrower_id int not null auto_increment primary key,
+address_id int,
 first_name varchar(50),
 last_name varchar(50),
 email varchar(50),
-phone varchar(50)
+phone varchar(50),
+foreign key (address_id) references address(address_id)
 );
 
 CREATE TABLE book_loan
@@ -14,8 +25,13 @@ CREATE TABLE book_loan
 (loan_id int not null auto_increment primary key,
 borrower_id int NULL,
 loan_date date,
-foreign key (borrower_id) references borrower(borrower_id));
+foreign key (borrower_id) references borrower(borrower_id)
+);
 
 ALTER TABLE book_loan
 ADD COLUMN book_id int,
 ADD foreign key (book_id) references book (book_id);
+
+ALTER TABLE borrower
+ADD COLUMN address_id int,
+ADD foreign key (borrower_id) references borrower(borrower_id);
