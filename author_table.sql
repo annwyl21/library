736fefbf6f5b9d1403ba6fa243731b8ffcea39e2
Salma <salmaibrahim813@gmail.com>
2023-03-19 21:19:36 +0000

    removed id inside value to see if table runs

--- author_table.sql
+++ author_table.sql
@@ -12,22 +12,22 @@ title varchar(10)
 -- inserting data into author table 
 INSERT INTO author(first_name, last_name, title)
 values
-(1, 'Caroline', 'Criado Perez', 'OBE'),
-(2, 'Atul', 'Gawande', 'MD'),
-(3, 'Larry', 'Beasley', 'CM'),
-(4, 'Quanta A', 'Ahmed', 'MD'),
+('Caroline', 'Criado Perez', 'OBE'),
+('Atul', 'Gawande', 'MD'),
+('Larry', 'Beasley', 'CM'),
+('Quanta A', 'Ahmed', 'MD'),
 -- the first four authors are for Ellen's book data
-(5, 'Anthony', 'Powell', 'CBE'),
+('Anthony', 'Powell', 'CBE'),
 -- Anthony Powell wrote a book called 'A Question of Upbringing, Books Do Furnish a Room'
-(6, 'David', 'Olusoga', 'CBE'),
+('David', 'Olusoga', 'CBE'),
 -- Davd Olusoga wrote a book called 'Black and British. A Forgotten History'
-(7, 'Roald', 'Dahl', ' '),
+('Roald', 'Dahl', ' '),
 -- Roald Dahl wrote 'The BFG', 'Matilda', 'The Enormous Crocodile' 
-(8, 'Emily', 'Brontë', ' '),
+('Emily', 'Brontë', ' '),
 -- Emily Brontë wrote the classic 'Wuthering Heights' 
-(9, 'Rabindranath', 'Tagore', ' '),
+('Rabindranath', 'Tagore', ' '),
 -- Rabindranath Tagore wrote a book called 'Collected Poems and Plays of Rabindranath Tagore'
-(10, 'Malorie', 'Blackman', 'OBE');
+('Malorie', 'Blackman', 'OBE');
 -- Malorie Blackman wrote a book called 'Noughts and Crosses'
 SELECT*
 FROM author; 
