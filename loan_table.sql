0dfb99d43db2f88433c389e43166e25b72d43a99
Salma <salmaibrahim813@gmail.com>
2023-03-20 14:09:55 +0000

    added the borrower_id and changed the column position to have loan date as the last column

--- loan_table.sql
+++ loan_table.sql
@@ -12,6 +12,52 @@ values
   ('2023-04-10'),
   ('2023-03-25'),
   ('2023-03-13');
-  
+
+
+ 
+ update book_loan
+set borrower_id='1'
+where loan_id='1';
+
+update book_loan
+set borrower_id='2'
+where loan_id='2';
+
+update book_loan
+set borrower_id='3'
+where loan_id='3';
+
+update book_loan
+set borrower_id='4'
+where loan_id='4';
+
+update book_loan
+set borrower_id='5'
+where loan_id='5';
+
+update book_loan
+set borrower_id='6'
+where loan_id='6';
+
+update book_loan
+set borrower_id='7'
+where loan_id='7';
+
+update book_loan
+set borrower_id='8'
+where loan_id='8';
+
+update book_loan
+set borrower_id='9'
+where loan_id='9';
+
+update book_loan
+set borrower_id='10'
+where loan_id='10';
+
+
+
+ALTER TABLE book_loan MODIFY loan_date date AFTER book_id;
+
 SELECT *
 FROM book_loan;
