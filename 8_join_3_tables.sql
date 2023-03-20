b7ddd7862da58b55d7df8b98a7cd29d32c625831
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-20 18:48:47 +0000

    amend join

--- 8_join_3_tables.sql
+++ 8_join_3_tables.sql
@@ -1,7 +1,7 @@
 select
 	book_loan.loan_date as Overdue,
 	concat(borrower.first_name, ' ', borrower.last_name) as 'Borrower',
-    address.street as 'Street Address',
+    ifnull (address.street, 'address unknown') as 'Street Address',
     book.title as 'Book on Loan',
     concat(author.first_name, ' ', author.last_name, ' ', author.title) as Author
 from borrower
@@ -12,5 +12,6 @@ on borrower.address_id = address.address_id
 inner join book
 on book.book_id = book_loan.book_id
 inner join author
-on author.author_id = book.author_id
+on author.author_id = book.author_id;
 where loan_date < curdate();
+-- where address.street is null;
---
+++ replace_null_address.sql
@@ -0,0 +1,8 @@
+SELECT IFNULL(street, 'address unknown') FROM address;
+
+select
+	concat(borrower.first_name, ' ', borrower.last_name) as 'Borrower',
+    ifnull (address.street, 'address unknown') as 'Street Address'
+from borrower
+left outer join address
+on borrower.address_id = address.address_id;
