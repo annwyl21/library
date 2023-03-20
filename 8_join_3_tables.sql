47dcd43ed0fab7d07a4bedb817e97cdbd0f48c8e
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-20 18:06:10 +0000

    joins with Salma for overdue books

--- 8_join_3_tables.sql
+++ 8_join_3_tables.sql
@@ -1,11 +1,16 @@
 select
+	book_loan.loan_date as Overdue,
 	concat(borrower.first_name, ' ', borrower.last_name) as 'Borrower',
     address.street as 'Street Address',
-    book.title as 'Book on Loan'
+    book.title as 'Book on Loan',
+    concat(author.first_name, ' ', author.last_name, ' ', author.title) as Author
 from borrower
 inner join book_loan
 on book_loan.borrower_id = borrower.borrower_id
 left outer join address
 on borrower.address_id = address.address_id
 inner join book
-on book.book_id = book_loan.book_id;
+on book.book_id = book_loan.book_id
+inner join author
+on author.author_id = book.author_id
+where loan_date < curdate();
