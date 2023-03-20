5a660604bb9b02951637f7099a6414335c7dd2a4
Get into Tech <getintotech@gets-mbp.home>
2023-03-20 20:55:54 +0000

    mq book genre numbers

--- genre-bookgenre.sql
+++ genre-bookgenre.sql
@@ -36,6 +36,34 @@ ADD FOREIGN KEY (book_id) REFERENCES book (book_id);
 
 
 
+INSERT INTO book_genre (book_id, genre_id) 
+VALUES 
+(1, 11),
+(1, 12),  
+(2, 12),
+(3, 3),
+(3, 12),
+(4, 3),
+(4, 13),
+(5, 14),
+(6, 1),
+(7, 15),
+(7, 10),
+(7, 8),
+(8 , 15),
+(8 , 10),
+(8 , 8),
+(9, 16),
+(10, 9),
+(10, 17); 
+
+
+
+
+
+
+
+
 
 
 
