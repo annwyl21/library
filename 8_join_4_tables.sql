f0413e14ace297437a28cf683b697f4cb5eeb2be
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-21 16:48:28 +0000

    show me all the books categorised as adventure

---
+++ 8_join_4_tables.sql
@@ -0,0 +1,16 @@
+use library;
+
+-- show me all the books that are categoried as 'adventure'
+-- joins 4 tables
+select
+	book.title as "Book Title",
+    concat(author.first_name, ' ', author.last_name, ' ', author.title) as 'Author',
+    genre.genre_type as 'Genre'
+from genre
+inner join book_genre
+on book_genre.genre_id = genre.genre_id
+inner join book
+on book.book_id = book_genre.book_id
+inner join author
+on author.author_id = book.author_id
+where genre_type like '%Adventure%';
