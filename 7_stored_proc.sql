e123a1d5241e4c77f1972a480c462517373cef14
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-21 14:46:00 +0000

    add 2 stored procedures to file 7 to count books and library users

--- 7_stored_proc.sql
+++ 7_stored_proc.sql
@@ -1,7 +1,6 @@
 use library;
 
 -- attempt at creating a stored procedure to grab 3 rows of data
-
 DELIMITER //
 
 CREATE PROCEDURE SelectFirst3Books()
@@ -12,3 +11,33 @@ END //
 DELIMITER ;
 
 CALL SelectFirst3Books();
+
+
+
+
+-- stored procedure to count all the books in the library
+DELIMITER //
+
+CREATE PROCEDURE BookCount()
+BEGIN
+	SELECT count(*) from book;
+END //
+
+DELIMITER ;
+
+CALL BookCount();
+
+
+
+
+-- stored procedure to count the number of borrowers
+DELIMITER //
+
+CREATE PROCEDURE BorrowerCount()
+BEGIN
+	SELECT count(*) from borrower;
+END //
+
+DELIMITER ;
+
+CALL BorrowerCount();
