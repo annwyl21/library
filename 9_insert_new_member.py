095fb4a280aa03a89259c97afd2c1edf26ec80de
Ellen Houghton <ellen.a.ash@gmail.com>
2023-03-22 15:42:48 +0000

    stored procedure NewMember can now be called from the python file

--- 9_insert_new_member.py
+++ 9_insert_new_member.py
@@ -5,17 +5,17 @@ mycursor = cnx.cursor()
 
 firstn = 'Joseph'
 lastn = 'Smith'
-email = 'joseph.smith@schooltoday.com'
+email = 'joseph.smithschooltoday.com'
 cell_phone = '159753126'
 house = '24'
 building = 'Corringham'
-street = 'Craven Hill Gardens'
+street = 'CravenHillGardens'
 city = 'London'
-postcode = 'GN67 9NU'
+postcode = 'GN679NU'
 
-stored_proc=f'CALL NewMember({firstn}, {lastn}, {email}, {cell_phone}, {house}, {building}, {street}, {city}, {postcode});'
+args=[firstn, lastn, email, cell_phone, house, building, street, city, postcode]
 
-mycursor.execute(stored_proc)
+mycursor.callproc('NewMember', args)
 
 # tell me how many rows I inserted
 print(mycursor.rowcount, "record inserted.")
